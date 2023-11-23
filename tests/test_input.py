3028438b11803e3f1da140cc3b37c5cae1bae004
Nidal Iguer <hello@inidal.dev>
2023-11-23 15:46:15 -0700

    Update test_input.py

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,9 +1,11 @@
-   import unittest
-   from tympy import compare
+import unittest
+from tympy import compare
 
-   class TestCompare(unittest.TestCase):
-       def test_compare(self):
-           # Write your test here
 
-   if __name__ == '__main__':
-       unittest.main()
+class TestCompare(unittest.TestCase):
+    def test_compare(self):
+        pass  # Add this line
+
+
+if __name__ == '__main__':
+    unittest.main()
