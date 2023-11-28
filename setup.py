3c20658bf842a84795beed7eb27d71f69eaef9f2
Nidal Iguer <hello@inidal.dev>
2023-11-28 15:30:06 -0700

    Fix versioning control for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tympy',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(include=["tympy*"]),
     description='A Python library for observing output and execution times of scripts.',
     author='Nidal Iguer',
