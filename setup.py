8f6d12341d0f848553490884a77007652592d0bb
Nidal Iguer <hello@inidal.dev>
2023-11-28 15:38:48 -0700

    Update versioning for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tympy',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(include=["tympy*"]),
     description='A Python library for observing output and execution times of scripts.',
     author='Nidal Iguer',
